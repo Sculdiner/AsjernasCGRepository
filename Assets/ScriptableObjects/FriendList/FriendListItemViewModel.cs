377de15afdcc63d98b5b44f449414ee5f6b3450d
Sculdiner <lazalex.89@gmail.com>
2018-05-31 00:42:11 +0300

    forgot these..

---
+++ Assets/ScriptableObjects/FriendList/FriendListItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AsjernasCG.Common.BusinessModels;
+
+public class FriendListItemViewModel
+{
+    public int UserId;
+    public string Username;
+    public string StatusDescription;
+    public UserStatusModel Status;
+}
