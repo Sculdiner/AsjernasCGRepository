e048003f58c4641adbd2c4524b24792415065182
Sculdiner <lazalex.89@gmail.com>
2018-06-01 20:06:59 +0300

    moved files

---
+++ Assets/Scripts/Card/CardManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour {
+
+    public GameObject CardTemplatePrefab;
+
+    public TextAsset CardTemplates;
+
+    public GameObject CreateCard()
+    {
+        return null;
+    }
+}
