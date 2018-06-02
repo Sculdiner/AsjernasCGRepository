282781e20b28b6a6e77535150ab87049c8287f91
Evangelos Bobetsis <vag_bob@hotmail.com>
2018-06-02 20:55:27 +0300

    Installed DOTween, new scene for ingame-dragging purposes and refactored the two Prefab parameters(positions,managers, collider for drag etc.) as well as the 2 ScriptableObjects.

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System;
+
+namespace Scripts.Extensions
+{
+    public static class Extensions
+    {
+        public static class ThreadSafeRandom
+        {
+            [ThreadStatic]
+            private static System.Random Local;
+
+            public static System.Random ThisThreadsRandom
+            {
+                get { return Local ?? (Local = new System.Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+            }
+        }
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Logic/Deck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Scripts.Extensions;
+
+public class Deck : MonoBehaviour {
+
+    public List<CardAsset> cards = new List<CardAsset>();
+
+    void Awake()
+    {
+        cards.Shuffle();
+    }
+	
+}
